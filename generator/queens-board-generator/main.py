ceee11297cf3b05addbad844f37a86421d0c6dd0
Justin D. Harris <juharris@users.noreply.github.com>
2025-08-03 00:13:05 -0400

    Check more

--- generator/queens-board-generator/main.py
+++ generator/queens-board-generator/main.py
@@ -3,10 +3,14 @@ from src.solver import Solver
 
 
 def main():
+    # Make a board that can be solved deterministically.
     size = 4
     board = Board.from_size(size)
-    for r in range(size):
-        board.colors[r, :] = r
+    board.colors[0, :] = 1
+    board.colors[0, 1] = 0
+    board.colors[1, :] = 1
+    board.colors[2, :] = 2
+    board.colors[3, :] = 3
     solver = Solver(board)
     print(f"Board: {board}")
     solution = solver.solve()
--- generator/queens-board-generator/src/solver.py
+++ generator/queens-board-generator/src/solver.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
 
 import numpy as np
 from numpy.typing import NDArray
@@ -7,19 +9,50 @@ from numpy.typing import NDArray
 from .board import Board
 
 
+class SolutionLabel(Enum):
+    UNKNOWN = 0
+    NOT_QUEEN = 1
+    QUEEN = 2
+
+
+Solution = NDArray[np.int8]
+
+
 @dataclass
 class Solver:
     board: Board
 
-    def solve(self) -> NDArray[np.bool]:
-        result = np.zeros_like(self.board.colors, dtype=np.bool)
-        groups: defaultdict[np.int8,
-                            list[tuple[int, int]]] = defaultdict(list)
-        for i, row in enumerate(self.board.colors):
-            for j, col in enumerate(row):
-                groups[col].append((i, j))
+    def solve(self) -> Solution:
+        result = np.zeros_like(self.board.colors, dtype=SolutionLabel)
+        self._init_groups()
+        while not self._is_solved():
+            self.check_easys(result)
 
         return result
 
-    def check_easys(self):
-        pass
+    def _init_groups(self):
+        self.groups: defaultdict[np.int8,
+                                 set[tuple[int, int]]] = defaultdict(set)
+        for i, row in enumerate(self.board.colors):
+            for j, col in enumerate(row):
+                self.groups[col].add((i, j))
+
+    def _is_solved(self):
+        return len(self.groups) == 0
+
+    def check_easys(self, solution: Solution):
+        # TODO Check rows for only 1 unknown.
+        # TODO Check columns for only 1 unknown.
+        # TODO Check groups for only 1 value.
+        delete_color: Optional[np.int8] = None
+        for color, positions in self.groups.items():
+            if len(positions) == 1:
+                pos = next(iter(positions))
+                self.set_queen(solution, pos)
+                delete_color = color
+        if delete_color is not None:
+            del self.groups[delete_color]
+
+    def set_queen(self, solution: Solution, position: tuple[int, int]):
+        solution[position] = SolutionLabel.QUEEN.value
+        # TODO Clear rows, colums, adjacent, and groups.
